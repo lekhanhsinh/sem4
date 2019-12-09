5cb86844fbccbb013981dec7ea67a2e457098383
lekhanhsinhvn <38658239+lekhanhsinhvn@users.noreply.github.com>
2019-12-09 19:49:13 +0700

    hot fix

--- build/back-end-api/graphql/typeDefs/AuthEmployee.js
+++ build/back-end-api/graphql/typeDefs/AuthEmployee.js
@@ -5,6 +5,6 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var apollo_server_express_1 = require("apollo-server-express");
-var AuthEmployeeTypeDefs = apollo_server_express_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    extend type Mutation {\n        loginEmployee(email: String!, password: String!): UserType\n        logoutEmployee: String\n    }\n"], ["\n    extend type Mutation {\n        loginEmployee(email: String!, password: String!): UserType\n        logoutEmployee: String\n    }\n"])));
+var AuthEmployeeTypeDefs = apollo_server_express_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    extend type Mutation {\n        loginEmployee(email: String!, password: String!): EmployeeType\n        logoutEmployee: String\n    }\n"], ["\n    extend type Mutation {\n        loginEmployee(email: String!, password: String!): EmployeeType\n        logoutEmployee: String\n    }\n"])));
 exports.default = AuthEmployeeTypeDefs;
 var templateObject_1;
